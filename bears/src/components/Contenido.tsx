80efb84d71003f8ec1075fe9cfbd0cac3c7466cd
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-01-23 10:06:15 +0100

    arreglar overflow del contenido

--- bears/src/components/Contenido.tsx
+++ bears/src/components/Contenido.tsx
@@ -6,7 +6,7 @@ type Props = {
 }
 export const Contenido = ({ children }: Props) => {
     return (
-        <div className="relative flex flex-col items-center justify-between w-full min-h-screen p-8 ">
+        <div className="relative flex flex-col items-center justify-between w-full min-h-screen p-8 overflow-hidden">
 
             <div className="w-3/5 h-full">
                 {children}
