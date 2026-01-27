20469f8d7eb4a36ad5967fd6eb73f96e673801ea
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2026-01-27 17:07:45 +0100

    adornar un poco la page de cargando

--- bears/src/pages/Cargando.tsx
+++ bears/src/pages/Cargando.tsx
@@ -1,7 +1,7 @@
 
 const Cargando = () => {
     return (
-        <div>
+        <div className="flex items-center justify-between flex-1 w-screen h-screen text-4xl font-medium text-main">
             Cargando cotenido...
         </div>
     );
